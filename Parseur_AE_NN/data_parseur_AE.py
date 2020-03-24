45b1774f1920f6ce8ec3cda84aa5de3de21dc3e4
Nathanael BEAU <n.beau@LGOP3542.local>
2020-03-24 12:52:34 +0100

    add import

--- Parseur_AE_NN/data_parseur_AE.py
+++ Parseur_AE_NN/data_parseur_AE.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
 
+from Parseur_AE_simple.analysis_syntax_AE import Parser
+from Parseur_AE_simple.analysis_lexical_AE import Lexical
+
 class parseur_data():
 
     def __init__(self, data):
         self.data = data
-        
+
+
+
+
+
+
+if __name__ == "__main__":
+    test = Lexical('2 +     3 +5')
+    print(test.lexicalAnalysis())
